3ec13a7645a87421f003275e6612d39b82f50c4d
Viraj Parab <virajpa@thoughtworks.com>
2018-02-12 09:53:08 +0530

    Add exercise 2.3

---
+++ src/main/scala/Exercise2_3.scala
@@ -0,0 +1,16 @@
+object Exercise2_3 {
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (x:A) => (y: B) => f(x,y)
+  }
+
+  val add = (a:Int, b:Int) => a+b
+
+  def main(args: Array[String]): Unit = {
+
+      val c = curry(add)
+      val add2 = c(2)
+      val add3 = add2(3)
+      println(add3)
+  }
+}
